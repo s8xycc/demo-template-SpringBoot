7e634094f519675d48b042fad7b9a467b84c5197
“s8x” <xbc20030512@163.com>
2026-04-03 22:55:27 +0800

    初始化Demo项目

---
+++ src/main/java/com/s8x/demo/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.s8x.demo.controller;
+
+import com.s8x.demo.common.BaseResponse;
+import com.s8x.demo.common.ResultUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+
+    /**
+     * 健康检查
+     */
+    @GetMapping("/health")
+    public BaseResponse<String> health() {
+        return ResultUtils.success("ok");
+    }
+}
